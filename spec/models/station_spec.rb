0bd6d2ee5f58e054338cdda936548fce9c11641a
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-16 10:27:37 -0600

    Finished seperating and testing the model.

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
-require '../../app/models/station'
+require './app/models/station'
 
 describe Station do
   describe 'contains readable attribes' do
     it 'initializes with attribues' do
-      attrs = { name: 'Conoco',
-                address: '123 Easy Street',
-                fuel_type: 'ELEC',
+      attrs = { station_name: 'Conoco',
+                street_address: '123 Easy Street',
+                fuel_type_code: 'ELEC',
                 distance: '5.0',
-                access_time: '24 hours' }
-                
+                access_days_time: '24 hours' }
+
       station = Station.new(attrs)
 
-      expect(station.name).to eq(attrs[:name])
-      expect(station.name).to eq(attrs[:address])
-      expect(station.name).to eq(attrs[:fuel_type])
-      expect(station.name).to eq(attrs[:distance])
-      expect(station.name).to eq(attrs[:access_time])
+      expect(station.name).to eq(attrs[:station_name])
+      expect(station.address).to eq(attrs[:street_address])
+      expect(station.fuel_type).to eq(attrs[:fuel_type_code])
+      expect(station.distance).to eq(attrs[:distance])
+      expect(station.access_time).to eq(attrs[:access_days_time])
     end
   end
 end
