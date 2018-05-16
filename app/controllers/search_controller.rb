f2f89277e644484a7bb402c839ab61e30e815493
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-16 10:35:40 -0600

    Getting tests to pass. Starting to refactor station search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,23 +4,23 @@ class SearchController < ApplicationController
   end
 end
 
-class StationsSearch
-
-  def initialize(zip)
-    @zip = zip
-  end
-
-  def stations
-    conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=s7uscHW04X8llSdR1VVvLXgSsfeOMmfC3mTpeOrJ&location=#{@zip}&radius=6.0&fuel_type=ELEC,LPG")
-
-    request = conn.get { |req| }
-
-    raw_stations = JSON.parse(request.body, symbolize_names: true)[:fuel_stations]
-    stations = raw_stations.map do |station|
-      Station.new(station)
-    end
-
-    sorted_stations = stations.sort_by { |station| station.distance }
-    sorted_stations[0..9]
-  end
-end
+# class StationsSearch
+#
+#   def initialize(zip)
+#     @zip = zip
+#   end
+#
+#   def stations
+#     conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=s7uscHW04X8llSdR1VVvLXgSsfeOMmfC3mTpeOrJ&location=#{@zip}&radius=6.0&fuel_type=ELEC,LPG")
+#
+#     request = conn.get { |req| }
+#
+#     raw_stations = JSON.parse(request.body, symbolize_names: true)[:fuel_stations]
+#     stations = raw_stations.map do |station|
+#       Station.new(station)
+#     end
+#
+#     sorted_stations = stations.sort_by { |station| station.distance }
+#     sorted_stations[0..9]
+#   end
+# end
--- spec/services/stations_search_spec.rb
+++ spec/services/stations_search_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe 'Station Search' do
   describe 'it returns an array of stations' do
     it 'passes area code as arguement' do
-      stations = StationSearch.new(80203).stations
+      stations = StationsSearch.new(80203).stations
 
-      expect(station.count).to eq(10)
-      expect(station.first.name). to eq('UDL')
-      expect(station.first.address). to eq('UDL')
-      expect(station.first.distance). to eq('UDL')
-      expect(station.first.fuel_type). to eq('ELEC')
-      expect(station.first.access_time). to eq('24 hours a day')
+      expect(stations.count).to eq(10)
+      expect(stations.first.name). to eq('UDR')
+      expect(stations.first.address). to eq('800 Acoma St')
+      expect(stations.first.distance). to eq(0.31422)
+      expect(stations.first.fuel_type). to eq('ELEC')
+      expect(stations.first.access_time). to eq('24 hours daily')
     end
   end
 end
