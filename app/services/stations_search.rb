cdb3df350da7fecdd7adf28c8a25b5e440b7c3dc
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-16 10:58:37 -0600

    Trying to build out tests for new service broken off specifically for requests.

--- app/services/stations_search.rb
+++ app/services/stations_search.rb
@@ -7,20 +7,14 @@ class StationsSearch
     sorted_stations[0..9]
   end
 
-  def conn
-    Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=s7uscHW04X8llSdR1VVvLXgSsfeOMmfC3mTpeOrJ&location=#{@zip}&radius=6.0&fuel_type=ELEC,LPG")
-  end
+  private
 
   def request
-    conn.get { |req| }
-  end
-
-  def raw_stations
-    JSON.parse(request.body, symbolize_names: true)[:fuel_stations]
+    StationRequestService.new(@zip).raw_stations
   end
 
   def station_objects
-    raw_stations.map do |station|
+    request.map do |station|
       Station.new(station)
     end
   end
@@ -29,3 +23,21 @@ class StationsSearch
     station_objects.sort_by { |station| station.distance }
   end
 end
+
+# class StationRequestService
+#   def initialize(zip)
+#     @zip = zip
+#   end
+#
+#   def conn
+#     Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=s7uscHW04X8llSdR1VVvLXgSsfeOMmfC3mTpeOrJ&location=#{@zip}&radius=6.0&fuel_type=ELEC,LPG")
+#   end
+#
+#   def request
+#     conn.get { |req| }
+#   end
+#
+#   def raw_stations
+#     JSON.parse(request.body, symbolize_names: true)[:fuel_stations]
+#   end
+# end
